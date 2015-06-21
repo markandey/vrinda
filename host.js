9d7a69bf1bd76f946833b4cc688cea84a1f90018
Markandey Singh <markande@yahoo-inc.com>
2015-06-21 13:21:34 -0700

    node 12 problems

--- host.js
+++ host.js
@@ -8,9 +8,9 @@ var http = require("http"),
 http.createServer(function(request, response) {
 
   var uri = url.parse(request.url).pathname
-    , filename = path.join(basePath, uri);
+    , filename = path.join(basePath+'', uri);
 
-  path.exists(filename, function(exists) {
+  fs.exists(filename, function(exists) {
     if(!exists) {
       response.writeHead(404, {"Content-Type": "text/plain"});
       response.write("404 Not Found\n");
