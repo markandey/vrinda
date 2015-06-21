0181de2be1f71ab71f2d1fbb64c51a271b38b319
Markandey Singh <markande@yahoo-inc.com>
2015-06-21 15:24:08 -0700

    update

--- cron.sh
+++ cron.sh
@@ -2,4 +2,4 @@
 cd /home/pi/vrinda
 git pull
 killall -TERM node
-node /home/pi/vrinda/host.js 8888 /home/pi/vrinda< /dev/null &
+/usr/local/bin/node /home/pi/vrinda/host.js 8888 /home/pi/vrinda< /dev/null &
