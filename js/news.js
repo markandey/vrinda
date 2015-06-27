aa0ff0926c74e23f08e331715e577e3215b68986
Markandey Singh <markande@yahoo-inc.com>
2015-06-27 14:08:02 -0700

    some more chnages

--- js/news.js
+++ js/news.js
@@ -6,32 +6,32 @@ var frames = {
     '100%': {
         transform: "translateY(100px)"
     }
-}
+};
 
-    function newspaper(news) {
-        var i = 0,
-            items = news.query.results.item.slice(0, 5),
-            count = items.length,
-            list = document.getElementById('newslist'),
-            li = '<li><div class="title">{title}</div><div class="desc">{description}</div></li>',
-            html = [];
-        for (i = 0; i < count; i++) {
-            html.push(li.replace('{title}', items[i].title).replace('{description}', items[i].description));
-        }
-        list.innerHTML = html.join('');
-        setTimeout(function() {
-            var scrollHeight = $('#newslist').height() - window.innerHeight;
-            var value = "translateY(-" + scrollHeight + "px)"
-            frames['100%'].transform=value;
-
-            $.keyframe.define([frames]);
-            $("#newslist").playKeyframe({
-                name: 'scrollanimation',
-                duration: '60s',
-                 delay: '1s',
-                 iterationCount: 'infinite',
-                timingFunction: 'linear'
-            });
-        }, 100);
-
+function newspaper(news) {
+    var i = 0,
+        items = news.query.results.item.slice(0, 5),
+        count = items.length,
+        list = document.getElementById('newslist'),
+        li = '<li><div class="title">{title}</div><div class="desc">{description}</div></li>',
+        html = [];
+    for (i = 0; i < count; i++) {
+        html.push(li.replace('{title}', items[i].title).replace('{description}', items[i].description));
     }
+    list.innerHTML = html.join('');
+    setTimeout(function() {
+        var scrollHeight = $('#newslist').height() - window.innerHeight;
+        var value = "translateY(-" + scrollHeight + "px)"
+        frames['100%'].transform = value;
+
+        $.keyframe.define([frames]);
+        $("#newslist").playKeyframe({
+            name: 'scrollanimation',
+            duration: '60s',
+            delay: '1s',
+            iterationCount: 'infinite',
+            timingFunction: 'linear'
+        });
+    }, 1000);
+
+}
