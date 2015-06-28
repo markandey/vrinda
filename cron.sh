595a208d2f1565883153879c9a2899b0f418fa96
Markandey Singh <mark.dawn@gmail.com>
2015-06-28 03:25:55 -0700

    fixing server

--- cron.sh
+++ cron.sh
@@ -3,5 +3,5 @@ cd /home/pi/vrinda
 git pull
 killall -TERM node
 #killall -TERM chromium 2>/dev/null;
-/usr/local/bin/node /home/pi/vrinda/host.js 8888 /home/pi/vrinda< /dev/null &
+/usr/local/bin/node /home/pi/vrinda/serverjs/host.js 8888 /home/pi/vrinda< /dev/null &
 #/usr/bin/chromium  --app=http://localhost:8888&
