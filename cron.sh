3ea3b66905fdb5902859a50db778181416072bfe
Markandey Singh <markande@yahoo-inc.com>
2016-05-13 19:15:07 -0700

    update

--- cron.sh
+++ cron.sh
@@ -2,6 +2,6 @@
 cd /home/pi/vrinda
 git pull
 killall -TERM node
-#killall -TERM chromium 2>/dev/null;
+killall -TERM chromium 2>/dev/null;
 /usr/local/bin/node /home/pi/vrinda/serverjs/host.js 8888 /home/pi/vrinda< /dev/null &
-#/usr/bin/chromium  --app=http://localhost:8888&
+/usr/bin/chromium  --app=http://localhost:8888&
