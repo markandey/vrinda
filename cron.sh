8bac91d8fe62c53e4d4aa86f2f73ba36352990ae
Markandey Singh <markandey.singh@gmail.com>
2017-03-16 20:35:35 -0700

    a

--- cron.sh
+++ cron.sh
@@ -1,7 +1,6 @@
 #refresh node
 cd /home/pi/vrinda
 git pull
-killall -TERM node
-killall -TERM chromium 2>/dev/null;
+pkill node
+pkill epiphany-browse
 /usr/local/bin/node /home/pi/vrinda/serverjs/host.js 8888 /home/pi/vrinda< /dev/null &
-/usr/bin/chromium  --app=http://localhost:8888&
